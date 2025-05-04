4969973a659152ddecca204faf094b2072f3f9b2
Juan Diego Rodriguez Yanquen <jdrodriguezy@academia.usbbog.edu.co>
2025-05-04 14:54:46 -0500

    Ajustes de nombres y otros cambios

--- JS/RegistroC.js
+++ JS/RegistroC.js
@@ -10,3 +10,33 @@ btnIngreso.addEventListener("click", () => {
 btnRegistro.addEventListener("click",()=>{
   container.classList.add("toggle");
 })
+
+//Validación de formulario
+document.getElementById('Ingreso').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evitar el envío del formulario por defecto
+
+  //Verificar si el formulario está completo
+  if (this.checkValidity()) {
+    // Si el formulario es válido, puedes realizar la acción deseada
+    alert('Inicio de sesión exitoso');
+  // Redirigir a la página de inicio
+    window.location.href = "/index.html";
+  }
+});
+
+document.getElementById('Registro').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evitar el envío del formulario por defecto
+
+  //Verificar si el formulario está completo
+  if (this.checkValidity()) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password === confirmPassword) {
+      alert('Registro exitoso');
+      window.location.href = "/index.html";
+    } else{
+      alert('Las contraseñas no coinciden');
+    }
+  }
+});
